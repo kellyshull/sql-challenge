ea68dd4d7dddad007707ea3a3ececa758af0cee8
Kelly Shull <kelly.t.shull@gmail.com>
2020-08-23 21:30:11 -0700

    schema update adding foregin keys

--- EmployeeSQL/schema.sql
+++ EmployeeSQL/schema.sql
@@ -10,7 +10,7 @@ CREATE TABLE IF NOT EXISTS departments
 
 CREATE TABLE IF NOT EXISTS employees
 (
-    emp_no INTEGER NOT NULL,
+    emp_no INTEGER NOT NULL UNIQUE,
     emp_title_id VARCHAR(30) NOT NULL,
     birth_date DATE NOT NULL,
     first_name VARCHAR(255) NOT NULL,
@@ -41,7 +41,8 @@ CREATE TABLE IF NOT EXISTS salaries
 CREATE TABLE IF NOT EXISTS titles
 (
     title_id VARCHAR(30) NOT NULL,
-    title VARCHAR(30) NOT NULL    
+    title VARCHAR(30) NOT NULL,
+    PRIMARY KEY(title_id)
 );
 
 
@@ -76,12 +77,13 @@ ALTER TABLE salaries
     MATCH SIMPLE
 ;
     
-ALTER TABLE titles
-    ADD    FOREIGN KEY (title_id)
-    REFERENCES employees(emp_title_id)
+ALTER TABLE employees
+    ADD    FOREIGN KEY (emp_title_id)
+    REFERENCES titles(title_id)
     MATCH SIMPLE
 ;
     
 
 -- Create Indexes
+
 
