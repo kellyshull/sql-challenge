5b7e18a2c1ce79640ee19cf27ea26eaeb46ffb19
Kelly Shull <kelly.t.shull@gmail.com>
2020-08-24 03:28:51 -0700

    queries 4 - 6

--- EmployeeSQL/SQL-Queries.sql
+++ EmployeeSQL/SQL-Queries.sql
@@ -1,4 +1,5 @@
 -- 1.) List the following details of each employee: employee number, last name, first name, sex, and salary.
+
 SELECT 
     employees.emp_no AS "employee number",
     employees.last_name AS "last name",
@@ -10,6 +11,7 @@ INNER JOIN salaries
 ON employees.emp_no=salaries.emp_no;
 
 -- 2.) List first name, last name, and hire date for employees who were hired in 1986.
+
 SELECT 
     employees.first_name AS "First Name",
     employees.last_name AS "Last Name", 
@@ -18,6 +20,7 @@ FROM employees
 WHERE hire_date >= '1986-01-01' AND hire_date <= '1986-12-31';
 
 -- 3.) List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+
 SELECT 
     dept_manager.dept_no AS "Department Number",
     departments.dept_name AS "Department Name",
@@ -31,12 +34,36 @@ INNER JOIN employees ON dept_manager.emp_no=employees.emp_no;
 
 -- 4.) List the department of each employee with the following information: employee number, last name, first name, and department name.
 
+SELECT
+    dept_emp.emp_no AS "Employee Number",
+    employees.last_name AS "Last Name",
+    employees.first_name AS "First Name",
+    departments.dept_name AS "Department"
+
+FROM employees
+INNER JOIN dept_emp ON employees.emp_no=dept_emp.emp_no
+INNER JOIN departments ON departments.dept_no=dept_emp.dept_no;
 
 -- 5.) List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
 
+SELECT
+    first_name AS "First Name",
+    last_name AS "Last Name",
+    sex AS "Sex"
+FROM employees WHERE first_name = 'Hercules' 
+AND last_name LIKE 'B%';
 
 --6.) List all employees in the Sales department, including their employee number, last name, first name, and department name.
 
+SELECT
+    employees.emp_no AS "Employee Number",
+    employees.last_name AS "Last Name",
+    employees.first_name AS "First Name",
+    departments.dept_name AS "Department Name"
+FROM employees 
+INNER JOIN dept_emp ON dept_emp.emp_no=employees.emp_no
+INNER JOIN departments ON dept_emp.dept_no=departments.dept_no
+WHERE departments.dept_name = 'Sales';
 
 --7.) List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
 
